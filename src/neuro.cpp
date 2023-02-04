10144b8df1e2da3d472b92a0655ea67307e346b8
Eryk Halicki <erykhalicki0@gmail.com>
2023-02-04 17:43:38 -0500

    fully functional reproduction

--- src/neuro.cpp
+++ src/neuro.cpp
@@ -1,4 +1,6 @@
 #include "neuro.h"
+#include "color.h"
+#include "position.h"
 #include <iostream>
 #include <chrono>
 #include <string>
@@ -26,17 +28,17 @@ network* init(int layers,int inputNeurons,int neuronsInLayer, float neuronMutati
 	n->neuronValues=(float*)malloc(inputNeurons+(layers-1)*neuronsInLayer*sizeof(float));
 	n->connectionNum=0;
 	n->neuronNum=0;
-	n->connections=(long*)malloc(sizeof(long)*((layers-2)*neuronsInLayer+inputNeurons));
+	n->connections=(u_long*)malloc(0);
 	srand(std::chrono::microseconds(std::chrono::system_clock::now().time_since_epoch()).count());//time since epoch as random seed
 	for(int i=0;i<inputNeurons;i++){
 		addNeuron(n,0,1);
-		n->connections[n->connectionNum++]=createConnection(n->neuronNum,0,rand()%neuronsInLayer,1);//connects each input neuron to a random neuron in layer 1
+		addConnection(n,createConnection(i,0,rand()%neuronsInLayer,1));//connects each input neuron to a random neuron in layer 1
 	}
 	for(int layer=1;layer<layers;layer++){//initializing arrays
 		for(int i=0;i<neuronsInLayer;i++){
 			addNeuron(n,layer,1);
 			if(layer>1){
-				n->connections[n->connectionNum++]=createConnection(i+neuronsInLayer*(layer-1)+inputNeurons,layer-1,i+neuronsInLayer*layer+inputNeurons,layer);
+				addConnection(n,createConnection(i,layer-1,i,layer));
 			}
 		}
 	}
@@ -45,42 +47,61 @@ network* init(int layers,int inputNeurons,int neuronsInLayer, float neuronMutati
 	}
 	return n;
 }
-network* init(network* original){
+network* init(network* original,bool blank){
 	network* n=(network*)malloc(sizeof(network));
 	n->neuronMutationRate=original->neuronMutationRate;
 	n->connectionMutationRate=original->connectionMutationRate;
 	n->neuronMutationStrength=original->neuronMutationStrength;
 	n->paramMutationRate=original->paramMutationRate;
 	n->layers=original->layers;
-	n->neuronNum=original->neuronNum;
-	n->connectionNum=original->connectionNum;
+	if(!blank){
+		n->neuronNum=original->neuronNum;
+		n->connectionNum=original->connectionNum;
+	}
+	else{
+		n->neuronNum=0;
+		n->connectionNum=0;
+	}
 	//malloc and then memcpy every pointer in the struct
 	n->layerNeuronCount=(int*)malloc(sizeof(int)*n->layers);
 	n->neuronWeights=(float*)malloc(sizeof(float)*n->neuronNum);
 	n->neuronValues=(float*)malloc(sizeof(float)*n->neuronNum);
-	n->connections=(long*)malloc(sizeof(long)*n->connectionNum);
-	memcpy((void*)original->layerNeuronCount,n->layerNeuronCount,sizeof(int)*n->layers);
-	memcpy((void*)original->neuronWeights,n->neuronWeights,sizeof(int)*n->neuronNum);
-	memcpy((void*)original->neuronValues,n->neuronValues,sizeof(int)*n->neuronNum);
-	memcpy((void*)original->connections,n->connections,sizeof(int)*n->connectionNum);	
+	n->connections=(u_long*)malloc(sizeof(long)*n->connectionNum);
+	if(!blank){
+		memcpy((void*)n->layerNeuronCount,original->layerNeuronCount,sizeof(int)*n->layers);
+		memcpy((void*)n->neuronWeights,original->neuronWeights,sizeof(float)*n->neuronNum);
+		memcpy((void*)n->neuronValues,original->neuronValues,sizeof(float)*n->neuronNum);
+		memcpy((void*)n->connections,original->connections,sizeof(long)*n->connectionNum);	
+	}
 	return n;
 }
 network* init(std::string file){
 	network* n=(network*)malloc(sizeof(network));	
 	return n;	
 }
-inline long createConnection(int fromAddress, int fromLayer, int toAddress, int toLayer){
-	long ans=fromAddress;	
-	ans<<=28;
-	ans|=fromLayer;
+u_long createConnection(u_int fromAddress, u_int fromLayer, u_int toAddress, u_int toLayer){
+	u_long ans=0;
+	ans|=fromAddress;	
 	ans<<=4;
-	ans|=toAddress;
+	ans|=fromLayer;
 	ans<<=28;
+	ans|=toAddress;
+	ans<<=4;
 	ans|=toLayer;
 	return ans;		
 }
-float eval(network n, Position pos){
+float eval(network* n, Position pos){
 	float ans;
+	u_long temp;
+	std::bitset<64> x(pos.pawns(Color::WHITE));
+	std::cout<<x;
+	for(int i=0;i<n->connectionNum;i++){
+		temp=n->connections[i];
+		*neuronAddressLocator(n,temp&0xF,temp>>4&0xFFFFFFF,true)+=*neuronAddressLocator(n,temp>>32&0xF,temp>>36,true)*(*neuronAddressLocator(n,temp&0xF,temp>>4&0xFFFFFFF,false));
+	}
+	for(int i=0;i<n->layerNeuronCount[n->layers-1];i++){
+		ans+=*neuronAddressLocator(n,n->layers-1,i,true);
+	}
 	return ans;
 }
 network* reproduce(network* n1,network* n2){
@@ -110,67 +131,94 @@ network* reproduce(network* n1,network* n2){
 	network* children=(network*)malloc(4*sizeof(network));
 	srand(std::chrono::microseconds(std::chrono::system_clock::now().time_since_epoch()).count());//time since epoch as random seed
 	int neuron=0,layerNeuron=0,n1offset=0,n2offset=0;
-	children[0]=*init(n1);
-	children[1]=*init(n2);
-	children[2]=*init(n1);
-	children[3]=*init(n2);
-	for(int layer=1;layer<n1->layers;layer++){//only works if both networks have same amount of layers
+	children[0]=*init(n1,true);
+	children[1]=*init(n2,true);
+	children[2]=*init(n1,false);
+	children[3]=*init(n1,false);
+	for(int layer=0;layer<n1->layers;layer++){//only works if both networks have same amount of layers
 		layerNeuron=0;
 		children[0].layerNeuronCount[layer]=0;
 		children[1].layerNeuronCount[layer]=0;
-		for(;layerNeuron<fmin(n1->layerNeuronCount[layer],n1->layerNeuronCount[layer]);layerNeuron++){
-			if(rand()%2){
-				children[0].neuronWeights[neuron]=n1->neuronWeights[neuron-n1offset];
-				children[1].neuronWeights[neuron]=n2->neuronWeights[neuron-n2offset];
+		for(;layerNeuron<fmin(n1->layerNeuronCount[layer],n2->layerNeuronCount[layer]);layerNeuron++){
+			if(rand()%2==0){
+				//children[0].neuronWeights[neuron]=n1->neuronWeights[neuron-n1offset];
+				//children[1].neuronWeights[neuron]=n2->neuronWeights[neuron-n2offset];
+				addNeuron(children,layer,n2->neuronWeights[neuron-n2offset]);
+				addNeuron(children+1,layer,n1->neuronWeights[neuron-n1offset]);
 			}
 			else{
-				children[0].neuronWeights[neuron]=n2->neuronWeights[neuron-n2offset];
-				children[1].neuronWeights[neuron]=n1->neuronWeights[neuron-n1offset];
+				//children[0].neuronWeights[neuron]=n2->neuronWeights[neuron-n2offset];
+				addNeuron(children+1,layer,n2->neuronWeights[neuron-n2offset]);
+				addNeuron(children,layer,n1->neuronWeights[neuron-n1offset]);
+				//children[1].neuronWeights[neuron]=n1->neuronWeights[neuron-n1offset];
 			}
 			neuron++;
-			children[0].layerNeuronCount[layer]++;
-			children[1].layerNeuronCount[layer]++;
 		}
 		for(;n1->layerNeuronCount[layer]>layerNeuron;layerNeuron++){
-			if(rand()%2){
-				children[0].neuronWeights[neuron]=n1->neuronWeights[neuron-n1offset];
-				children[1].neuronWeights[neuron]=n1->neuronWeights[neuron-n1offset];
-				children[0].layerNeuronCount[layer]++;
-				children[1].layerNeuronCount[layer]++;
+			if(rand()%2==0){
+				addNeuron(children,layer,n1->neuronWeights[neuron-n1offset]);
+				addNeuron(children+1,layer,n1->neuronWeights[neuron-n1offset]);
 				neuron++;
 				n2offset++;
 			}
 		}
 		for(;n2->layerNeuronCount[layer]>layerNeuron;layerNeuron++){
-			if(rand()%2){
-				children[0].neuronWeights[neuron]=n2->neuronWeights[neuron-n2offset];
-				children[1].neuronWeights[neuron]=n2->neuronWeights[neuron-n2offset];
-				children[0].layerNeuronCount[layer]++;
-				children[1].layerNeuronCount[layer]++;
+			if(rand()%2==0){
+				addNeuron(children,layer,n2->neuronWeights[neuron-n2offset]);
+				addNeuron(children+1,layer,n2->neuronWeights[neuron-n2offset]);
 				neuron++;
 				n1offset++;
 			}
 		}
-	}		
-	for(int c=0;c<fmin(n1->connectionNum,n2->connectionNum);c++){
-		if(rand()%2){
-			children[0].connections[c]=n1->connections[c];
-			children[1].connections[c]=n2->connections[c];
+	}	
+	int c=0;	
+	for(;c<fmin(n1->connectionNum,n2->connectionNum);c++){
+		if(rand()%2==0){
+			addConnection(children,n1->connections[c]);
+			addConnection(children+1,n2->connections[c]);
 		}
 		else{
-			children[1].connections[c]=n1->connections[c];
-			children[0].connections[c]=n2->connections[c];
+			addConnection(children,n2->connections[c]);
+			addConnection(children+1,n1->connections[c]);
+		}
+	}
+	for(;c<n1->connectionNum;c++){
+		if(rand()%2==0){
+			addConnection(children,n1->connections[c]);
+			addConnection(children+1,n1->connections[c]);
+		}
+	}
+	for(;c<n2->connectionNum;c++){	
+		if(rand()%2==0){
+			addConnection(children,n2->connections[c]);
+			addConnection(children+1,n2->connections[c]);
+		}
+	}
+	for(int i=0;i<children->connectionNum;i++){
+		if(!validConnection(children,children->connections[i])){
+			removeConnection(children,children->connections+i);		
+		}
+	}
+	for(int i=0;i<(children+1)->connectionNum;i++){
+		if(!validConnection(children+1,(children+1)->connections[i])){
+			removeConnection(children+1,(children+1)->connections+i);		
 		}
 	}
 	//add remaining connections
 	//using paramMutationRate because i am too lazy to add another mutation parameter
 	mutate(&children[0]);
 	mutate(&children[1]);
-	for(int i=0;i<n1->paramMutationRate+n2->paramMutationRate;i++){
+	int temp=rand()%20;
+	for(int i=0;i<temp;i++){
 		mutate(&children[2]);
 		mutate(&children[3]);
 	}	
 	return children;
+}
+bool validConnection(network* n, u_long connection){
+	if((connection>>4&0xFFFFFFF)>n->layerNeuronCount[connection&0xF])return false;
+	if((connection>>36)>n->layerNeuronCount[(connection>>32)&0xF])return false;		
+	return true;
 }
 void mutate(network* n){
 	/*inplace mutation function
@@ -194,14 +242,32 @@ void addNeuron(network* n, int layer, float weight){
 	n->neuronWeights=(float*)realloc(n->neuronWeights,sizeof(float)*n->neuronNum);
 	n->neuronValues=(float*)realloc(n->neuronValues,sizeof(float)*n->neuronNum);
 	float* temp=neuronAddressLocator(n,layer,n->layerNeuronCount[layer],true);
-	memmove((void*)temp,(void*)(temp+1),n->neuronValues+n->neuronNum-temp);
+	memmove((void*)(temp+1),(void*)(temp),n->neuronValues+n->neuronNum-temp);
 	*temp=0;
 	temp=neuronAddressLocator(n,layer,n->layerNeuronCount[layer],false);
-	memmove((void*)temp,(void*)(temp+1),n->neuronWeights+n->neuronNum-temp);
+	memmove((void*)(temp+1),(void*)(temp),n->neuronWeights+n->neuronNum-temp);
 	*temp=weight;
 	n->layerNeuronCount[layer]++;
 }//create similar addConnection function, possibly replacing createConnection
- //this also forces a slight change to the initial connection creations
+void addConnection(network* n, u_long connection){
+	n->connectionNum++;
+	n->connections=(u_long*)realloc(n->connections,sizeof(u_long)*n->connectionNum);
+	n->connections[n->connectionNum-1]=connection;
+}//this also forces a slight change to the initial connection creations
+void removeNeuron(network* n, int layer, int relativeAddress){
+	n->neuronNum--;
+	float* temp=neuronAddressLocator(n,layer,relativeAddress,true);
+	memmove((void*)temp,(void*)(temp-1),n->neuronValues+n->neuronNum-temp);
+	temp=neuronAddressLocator(n,layer,relativeAddress,false);
+	memmove((void*)temp,(void*)(temp-1),n->neuronWeights+n->neuronNum-temp);
+	n->neuronWeights=(float*)realloc(n->neuronWeights,sizeof(float)*n->neuronNum);
+	n->neuronValues=(float*)realloc(n->neuronValues,sizeof(float)*n->neuronNum);
+	n->layerNeuronCount[layer]--;
+}
+void removeConnection(network* n, u_long* loc){
+	memmove((void*)loc,(void*)(loc+1),n->connections+n->connectionNum-loc);	
+	n->connectionNum--;
+}
 float* neuronAddressLocator(network* n, int layer, int relativeAddress, bool value){
 	int addressOffset=0;
 	for(int l;l<layer;l++){
@@ -213,19 +279,39 @@ float* neuronAddressLocator(network* n, int layer, int relativeAddress, bool val
 	if(value)return n->neuronValues+addressOffset;
 	return n->neuronWeights+addressOffset;
 }
-void printInfo(network* n){
-	std::cout<<"layers: "<<n->layers<<'\n';	
+void printInfo(network* n, bool verbose){
+	std::cout<<"layers: "<<n->layers<<'\n';
 	for(int i=0;i<n->layers;i++){
 		std::cout<<"layer "<<i<<" neurons: "<<n->layerNeuronCount[i]<<'\n';	
+		if(verbose){	
 		for(int j=0;j<n->layerNeuronCount[i];j++){
 			std::cout<<"neuron "<<i<<","<<j<<" Weight: "<<*neuronAddressLocator(n,i,j,false)<<" Value: "<<*neuronAddressLocator(n,i,j,true)<<'\n';
 		}
+		}
 	}
+
+	if(verbose){	
+	for(int i=0;i<n->connectionNum;i++){
+		std::cout<<"connection "<<i<<" "<<"to_layer: "<<(n->connections[i]&0xF)<<'\n';
+		std::cout<<"connection "<<i<<" "<<"to_address: "<<((n->connections[i]>>4)&0xFFFFFFF)<<'\n';
+		std::cout<<"connection "<<i<<" "<<"from_layer: "<<((n->connections[i]>>32)&0xF)<<'\n';
+		std::cout<<"connection "<<i<<" "<<"from_address: "<<((n->connections[i]>>36))<<'\n'<<'\n';
+	}
+	}
+	std::cout<<"connections: "<<n->connectionNum<<'\n';
+}
+float similiarity(network* n1,network* n2){
+	float ans;
+	return ans;
 }
 void test(){
 	network* n1=init(5,50,10,0.5,0.5,1,0.1, 10);
-	//network* n2=init(5,50,10,0.5,0.5,1,0.1, 10);
-	//network* children=reproduce(n1,n2);
-	printInfo(n1);
+	network* n2=init(5,50,20,0.5,0.5,1,0.1, 10);
+	network* children=reproduce(n1,n2);
+	printInfo(&children[0],false);
+	printInfo(&children[1],false);
+	printInfo(&children[2],false);
+	printInfo(&children[3],false);
+	std::cout<<eval(n1,Position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))<<"\n";
 }
 }
--- src/neuro.h
+++ src/neuro.h
@@ -10,10 +10,10 @@ namespace neuro{
 	       	int* layerNeuronCount;//number of neurons per layer, max 2^28  
 		float* neuronWeights;//array of floats for weights and values of each node, maxvalue floatmax
 		float* neuronValues;
-		long* connections;//64 bit data type, 28 bits for id, 4 bits for layer number, 32 for input 32 for output
+		u_long* connections;//64 bit data type, 28 bits for id, 4 bits for layer number, 32 for input 32 for output
 	};
 	
-	long createConnection(int fromAddress, int fromLayer, int toAddress, int toLayer);
+	u_long createConnection(u_int fromAddress, u_int fromLayer, u_int toAddress, u_int toLayer);
 	network* init(int layers,int inputNeurons,int neuronsInLayer, float neuronMutationRate,float connectionMutationRate,float neuronMutationStrength,float paramMutationRate, int preMutationAmount);//network generation from scratch 
 	network* init(network* original);//clone network to new pointer
 	network* init(std::string file);//load network from file
@@ -22,6 +22,10 @@ namespace neuro{
 	network* reproduce(network n1, network n2);//returns 4 children, 2 mutated inverse crossover children, 1 mutated n1, 1 mutated n2  
 	void mutate(network* n);//inplace mutation function, uses networks own mutation hyperparameters
 	void addNeuron(network* n, int layer, float weight);
+	void addConnection(network* n, u_long connection);
+	bool validConnection(network* n, u_long connection);
+	void removeConnection(network* n, u_long* loc);
+	void removeNeuron(network* n, int layer, int relativeAddress);
 	float* neuronAddressLocator(network* n, int layer, int relativeAddress, bool value);
 	void printInfo(network* n);
 	void test();//test all functions	
